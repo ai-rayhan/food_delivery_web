f57ca7ab8486806ff16b23b0ce600afaabd97748
CmtBoy <alaminislam.dev@gmail.com>
2023-04-12 03:40:28 +0600

    product add and delete system added

--- lib/screens/admin/add_product.dart
+++ lib/screens/admin/add_product.dart
@@ -1,15 +1,136 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({super.key});
 
   @override
-  State<AddProduct> createState() => _AddProductState();
+  _AddProductState createState() => _AddProductState();
 }
 
 class _AddProductState extends State<AddProduct> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _priceController = TextEditingController();
+  final _imageUrlController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  Future<void> _submitForm() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    final name = _nameController.text;
+    final price = double.parse(_priceController.text);
+    final imageUrl = _imageUrlController.text;
+    final description = _descriptionController.text;
+    try {
+      await FirebaseFirestore.instance
+          .collection('products')
+          .doc('allproduct')
+          .get()
+          .then((docSnapshot) {
+        if (docSnapshot.exists) {
+          List<dynamic> prod = docSnapshot.data()!['productdetails'];
+          prod.insert(0, {
+            'name': name,
+            'price': price,
+            'description': description,
+            'image': imageUrl,
+            "id": DateTime.now().toString()
+          });
+          FirebaseFirestore.instance
+              .collection('products')
+              .doc('allproduct')
+              .update({'productdetails': prod});
+        }
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Product added successfully'),
+        ),
+      );
+      // clear the form
+      _formKey.currentState!.reset();
+    } catch (error) {
+      // show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to add product'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(160.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                controller: _nameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                controller: _priceController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a price';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid price';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Image URL'),
+                controller: _imageUrlController,
+                keyboardType: TextInputType.url,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter an image URL';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                controller: _descriptionController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
